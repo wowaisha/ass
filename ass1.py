970897b7832dc88d572ecb308141cb7d0ca90d05
wowaisha <wow.aisha24@gmail.com>
2025-03-27 22:40:58 +0100

    Update added question1.2

--- ass1.py
+++ ass1.py
@@ -1,9 +1,10 @@
-import math,random  #importalja a a math es random modulokat
+import math,random,time  #importalja a a math es random modulokat
 
 feature1 = ['könyvtáros', 'tanár']  #meghatarozza az elso lehetseges szot
 feature2 = ['csendes', 'cserfes']  #meghatarozza a masodik lehetseges szot
 operator = [' és ', ' vagy ']  #az elso es masodik szo kozti op
 dyslexia_prob = 0.5  #a diszle
+misinterpret_prob = 0.8
 
 
 def uniformDraw(array, chance = True):
@@ -35,5 +36,42 @@ def ComplexModel1():
   
   return ervenyes
 
+def uniformDraw(array, chance = True):
+    prob_number = random.random()
+    if chance == True:
+        return array[math.floor(random.random() * len(array))]
+    else:
+        value = ' vagy ' if 0.95 > prob_number else ' és '
+        return value
+    
+
+def ComplexModel2():
+  word1 = uniformDraw(feature1)
+  op = uniformDraw(operator, False)
+  if op == ' vagy ':
+      op = ' vagy ' if random.random() > misinterpret_prob else ' és '
+  word2 = uniformDraw(feature2)
+  if (random.random() < dyslexia_prob):
+    word2 = 'cserfes' if (word2 == 'csendes') else 'csendes'
+
+  print('Premissza: Panni ' + word1 + op + word2 + '.')
+
+  word3 = uniformDraw(feature2);
+  print('Konklúzió: Panni ' + word3 + '.')
+
+  if (op == ' és '):
+    ervenyes = 'érvényes' if (word2 == word3) else 'nem érvényes'
+  else:
+    ervenyes = 'érvényes'
+
+  print(ervenyes);
+  
+  return ervenyes
+
+print('ComplexModel1 Tölt...')
 ComplexModel1()
+print('ComplexModel1 Vege. Kerem varjon par masodpercet')
+time.sleep(5)
+print('ComplexModel2 Tölt...')
+ComplexModel2()
 
