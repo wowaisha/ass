fe83916f1fca95bb77853ae62c5b986ca374cd44
wowaisha <wow.aisha24@gmail.com>
2025-03-31 22:10:00 +0200

    Update ass3.py

--- ass3.py
+++ ass3.py
@@ -1,9 +1,6 @@
-from math import comb
-
-def probability_king_and_non_king():
-    total_outcomes = comb(52, 2) #összes lehetséges kértya 
-    favorable_outcomes = comb(4, 1) * comb(48, 1) # 1 király és 1 nem király
-    probability = favorable_outcomes / total_outcomes # valószínűség = kedvező / összes
-    return probability
-
-print(f"Annak a valószínűsége, hogy az egyik kártya király, a másik nem király: {probability_king_and_non_king():.4f}")
+def probability_king_not_king():
+    favorable_cases = 4 * 48  #kedvezó eset, ha 4 király és 48 nem király
+    total_cases = (52 * 51) // 2  #2 kártya húzása, összesen 52 lap
+    probability = favorable_cases / total_cases # return probability
+result = probability_king_not_king()
+print(f"A valószínűsége, hogy az egyik kártya király, a másik pedig nem király: {result:.4f}")
